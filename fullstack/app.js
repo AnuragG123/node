3d4909d86790169cb0257a9959067d9384095a42
aakashDeveloper <ahanda205@gmail.com>
2020-04-29 16:18:20 +0100

    a

--- fullstack/app.js
+++ fullstack/app.js
@@ -2,18 +2,27 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8900;
 
+var restaurantsRouter = require('./src/routes/restaurantRouter')
+var cityRouter = require('./src/routes/cityRouter');
+var cors = require('cors');
+app.use(cors())
+
+//Static File Path
+app.use(express.static(__dirname+'/public'))
+//HTML
+app.set('views', './src/views')
+//View Engine
+app.set('view engine', 'ejs')
+
+
 //Routes
 app.get('/',function(req,res){
-    res.status(200).send("<h1> Welcome to NodeJs</h1>")
+    //res.status(200).send("<h1> Welcome to NodeJs</h1>")
+    res.render('index')
 });
 
-app.get('/restaurants', function(req,res){
-    res.status(200).send("<h1> Welcome to Restaurants</h1>")
-})
-
-app.get('/city',function(req,res){
-    res.status(200).send("<h1> Welcome to City</h1>")
-})
+app.use('/restaurants', restaurantsRouter);
+app.use('/city',cityRouter);
 
 app.listen(port, function(err){
     if(err) throw err;
